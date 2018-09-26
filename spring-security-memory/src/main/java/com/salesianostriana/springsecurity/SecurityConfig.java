cb09b2b5a1c716d9dfae4691fb4bb6b4b351db6c
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2018-09-26 19:25:59 +0200

    Ejemplo de Spring Security con autenticación en memoria

---
+++ spring-security-memory/src/main/java/com/salesianostriana/springsecurity/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.salesianostriana.springsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@SuppressWarnings("deprecation")
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private LoggingAccessDeniedHandler accessDeniedHandler;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off		
+	        http
+	                .authorizeRequests()
+	                    .antMatchers(
+	                            "/",
+	                            "/js/**",
+	                            "/css/**",
+	                            "/img/**",
+	                            "/webjars/**").permitAll()
+	                    .antMatchers("/user/**").hasRole("USER")
+	                    .anyRequest().authenticated()
+	                .and()
+	                .formLogin()
+	                    .loginPage("/login")
+	                    .permitAll()
+	                .and()
+	                .logout()
+	                    .invalidateHttpSession(true)
+	                    .clearAuthentication(true)
+	                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+	                    .logoutSuccessUrl("/login?logout")
+	                    .permitAll()
+	                .and()
+	                .exceptionHandling()
+	                    .accessDeniedHandler(accessDeniedHandler);
+	     // @formatter:on
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// @formatter:off
+		auth
+				.inMemoryAuthentication()
+				.passwordEncoder(NoOpPasswordEncoder.getInstance())
+				.withUser("user").password("password").roles("USER")
+			.and()
+				.withUser("manager").password("password").roles("MANAGER");		 
+		// @formatter:on
+
+	}
+
+}
---
+++ spring-security-memory/src/main/java/com/salesianostriana/springsecurity/SpringSecurityMemoryApplication.java
@@ -0,0 +1,21 @@
+package com.salesianostriana.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * @author https://memorynotfound.com
+ * 
+ * @see https://memorynotfound.com/spring-boot-spring-security-thymeleaf-form-login-example/
+ * 
+ * Este tutorial se ha subido a este repositorio para facilitar su acceso a los alumnos de
+ * 2º DAM de Salesianos Triana.
+ *
+ */
+@SpringBootApplication
+public class SpringSecurityMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMemoryApplication.class, args);
+	}
+}

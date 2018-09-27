8f87654704066ef5125257d2adfe816460980878
Luis Miguel López Magaña <lmlopez@aulas.triana>
2018-09-27 18:06:18 +0200

    Algunos cambios

--- securitysample/src/main/java/com/salesianostriana/bd/securitysample/config/WebMvcConfiguration.java
+++ securitysample/src/main/java/com/salesianostriana/bd/securitysample/config/WebMvcConfiguration.java
@@ -23,7 +23,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/login").setViewName("login.html");;
+		registry.addViewController("/login").setViewName("login.html");
 	}
 
 }
--- securitysample/src/main/resources/data.sql
+++ securitysample/src/main/resources/data.sql
@@ -1,8 +1,11 @@
 insert into users(username,password,enabled)
 	values('admin','$2a$04$dJ5YWbG5dE0wuPaPZWQDHu2oM/K2luEFhxsztxM6jhNsaU4iT8lvO',true);
-	
+insert into users(username,password,enabled)
+	values('luismi','$2a$04$QOIfbf3aoT9XG2iEcSOKqubufKkhcPF396YSNZUmX3YO5KX.EvfLK',true);	
 -- Se puede generar la contraseña en BCrypt en https://www.dailycred.com/article/bcrypt-calculator 
 
 	
 insert into authorities(username,authority) 
 	values('admin','ROLE_ADMIN');
+insert into authorities(username,authority)
+	values('luismi','ROLE_USER');
